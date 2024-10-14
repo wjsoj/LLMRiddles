5c8556b6168272edb21f98ae200bc8a81f910fde
wjsoj <wjs@wjsphy.top>
2024-10-14 18:24:54 +0800

    update zhipuAI

--- llmriddles/llms/chatglm.py
+++ llmriddles/llms/chatglm.py
@@ -1,13 +1,13 @@
-import zhipuai
+from zhipuai import ZhipuAI
 from .base import register_llm
 
 
 def ask_chatglm(message: str, api_key: str):
-    zhipuai.api_key = api_key
+    client = ZhipuAI(api_key=api_key)
 
-    response = zhipuai.model_api.invoke(
-        model="chatglm_turbo",
-        prompt=[{
+    response = client.chat.completions.create(
+        model="glm-4-air",
+        messages=[{
             "role": "user",
             "content": message
         }],
@@ -15,7 +15,7 @@ def ask_chatglm(message: str, api_key: str):
         temperature=0.9,
     )
 
-    response_msg = response['data']['choices'][0]['content']
+    response_msg = response.choices[0].message.content
     # strip the front and end '"'
     if len(response_msg) >= 2:
         response_msg = response_msg[1:-1]
